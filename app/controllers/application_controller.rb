e1ea55ef4266d9a8cf89c476a71baf1b7429cb8e
MannaPriyanka <mannapriyanka04@gmail.com>
2024-11-08 00:07:29 +0530

    Error handling for common errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def user_not_authorized
+    render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
+  end
 end
